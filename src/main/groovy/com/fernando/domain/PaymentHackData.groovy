39a526a260c06eee94b75ab6b2ab599258263d8b
Fernando <fernando88to@gmail.com>
2022-07-11 17:44:36 -0300

    funcionando

---
+++ src/main/groovy/com/fernando/domain/PaymentHackData.groovy
@@ -0,0 +1,33 @@
+package com.fernando.domain
+
+class PaymentHackData {
+    String comarca
+    String cidade
+    String nomeCartorio
+    String atoNomeacao
+    String dataNomeacao
+    String corregedor
+    String dataCorreicao
+
+
+
+    String subtotal
+    String tax
+    String transform
+    String other
+    String unpay
+    String total
+
+    List<Goods> goods
+    List<Goods> goods2
+    List<Labor> labors
+    List<Labor> labors2
+
+
+    List<EquipeCorreicao> equipe
+
+    
+}
+
+
+
